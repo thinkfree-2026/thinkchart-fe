dd4cf4cd53c740bfb55450e4581a66f902587cd3
10hajin15 <10hajin15@gmail.com>
2026-04-28 10:11:28 +0900

    refactor: 함수명 수정

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -4,40 +4,46 @@ type ModalProps = {
   children?: HTMLElement;
 };
 
+export const openModal = () => {
+  const modal = document.getElementById('modal') as HTMLDialogElement | null;
+  modal?.showModal();
+};
+
+export const closeModal = () => {
+  const modal = document.getElementById('modal') as HTMLDialogElement | null;
+  modal?.close();
+};
+
 export const Modal = ({ width = 'w-[1100px]', height = 'h-[720px]', children }: ModalProps) => {
-  const onClickedCloseModal = () => {
-    const modal = document.getElementById('modal') as HTMLDialogElement | null;
-    modal?.close();
+  const handleCloseButtonClick = () => {
+    closeModal();
   };
 
-  const onCloseModal = (e: MouseEvent) => {
-    const modal = document.getElementById('modal') as HTMLDialogElement | null;
-
-    if (!modal) return;
+  const handleModalBackdropClick = (e: MouseEvent) => {
+    const modal = e.currentTarget as HTMLDialogElement;
 
     const rect = modal.getBoundingClientRect();
 
-    const inside =
-      rect.top <= e.clientY &&
-      e.clientY <= rect.top + rect.height &&
-      rect.left <= e.clientX &&
-      e.clientX <= rect.left + rect.width;
+    const isClickOutside =
+      e.clientY < rect.top || e.clientY > rect.bottom || e.clientX < rect.left || e.clientX > rect.right;
 
-    if (!inside) modal.close();
+    if (isClickOutside) {
+      closeModal();
+    }
   };
 
   return (
     <dialog
       id="modal"
-      className={`${width} ${height} relative m-auto max-h-[90vh] max-w-[95vw] rounded-[24px] bg-white/80 p-10 shadow-2xl backdrop:bg-black/40 backdrop:backdrop-blur-[2px]`}
-      onclick={onCloseModal}
+      className={`${width} ${height} relative m-auto max-h-[90vh] max-w-[95vw] rounded-[24px] bg-white/80 p-10 shadow-2xl backdrop:bg-black/40 backdrop:backdrop-blur-xs`}
+      onclick={handleModalBackdropClick}
     >
       <button
         id="btn-close-modal"
         type="button"
         aria-label="Close modal"
         className="absolute top-6 right-6 h-10 w-10 cursor-pointer rounded-full hover:bg-zinc-300"
-        onclick={onClickedCloseModal}
+        onclick={handleCloseButtonClick}
       >
         ✕
       </button>
