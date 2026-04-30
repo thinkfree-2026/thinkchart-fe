81abd4bd6c4363eb2ee156aa3e111066e2b98842
Donghyun Lee <61397627+shubug1015@users.noreply.github.com>
2026-04-30 13:51:33 +0900

    docs: createEffect 유틸리티 JSDoc 작성

--- src/utils/core/effect.ts
+++ src/utils/core/effect.ts
@@ -1,6 +1,25 @@
 const initializeCallbacks = new Map<Node, (el: Node) => void | (() => void)>();
 const cleanupCallbacks = new Map<Node, () => void>();
 
+/**
+ * @description
+ * DOM 변경을 감지해 Initialize 또는 Cleanup 콜백 함수를 실행하는 유틸리티
+ *
+ * @param {Node} node - 변경을 감지할 노드
+ * @param {(element: Node) => void | (() => void)} callback - 변경 감지 후 실행할 콜백 함수
+ */
+export const createEffect = (node: Node, callback: (element: Node) => void | (() => void)) => {
+  if (node.isConnected) {
+    const onCleanup = callback(node);
+
+    if (typeof onCleanup === 'function') {
+      cleanupCallbacks.set(node, onCleanup);
+    }
+  } else {
+    initializeCallbacks.set(node, callback);
+  }
+};
+
 const observer = new MutationObserver(mutations => {
   mutations.forEach(mutation => {
     mutation.addedNodes.forEach(node => {
@@ -26,15 +45,3 @@ const observer = new MutationObserver(mutations => {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
-
-export const createEffect = (node: Node, callback: (el: Node) => void | (() => void)) => {
-  if (node.isConnected) {
-    const onCleanup = callback(node);
-
-    if (typeof onCleanup === 'function') {
-      cleanupCallbacks.set(node, onCleanup);
-    }
-  } else {
-    initializeCallbacks.set(node, callback);
-  }
-};
