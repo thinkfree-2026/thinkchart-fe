3604a02375e3efb7c645ef81c7a8f1819992d077
Donghyun Lee <shubug@naver.com>
2026-05-03 17:51:22 +0900

    feat: 원 타입 작성

---
+++ src/canvas/types/circle.ts
@@ -0,0 +1,9 @@
+export type Circle = {
+  x: number; // 원의 X 좌표
+  y: number; // 원의 Y 좌표
+  size: number; // 원의 크기
+  r: number; // 원의 R 값
+  g: number; // 원의 G 값
+  b: number; // 원의 B 값
+  a: number; // 원의 A 값
+};
