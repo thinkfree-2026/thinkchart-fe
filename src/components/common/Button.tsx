f4f54ef8aba5966e951c909da85b5cf75b245bec
Hajin Lee <116067240+10hajin15@users.noreply.github.com>
2026-05-08 16:51:25 +0900

    feat: 버튼 컴포넌트 구현 (#46)
    * feat: Button 컴포넌트
    
    * chore: 변수명 수정

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,41 @@
+type ButtonColor = 'primary' | 'secondary' | 'outline';
+
+type ButtonProps = {
+  label: string;
+  type?: 'button' | 'submit';
+  color?: ButtonColor;
+  onClick: (e: MouseEvent) => void;
+};
+
+const COLOR_CLASS: Record<ButtonColor, string> = {
+  primary: `
+    bg-primary text-white
+    hover:brightness-105
+    shadow-lg
+  `,
+
+  secondary: `
+    bg-secondary text-white
+    hover:brightness-105
+    shadow-md
+  `,
+
+  outline: `
+    bg-transparent text-primary
+    hover:bg-primary/10
+    shadow-sm
+    border
+  `,
+};
+
+export const Button = ({ label, type = 'button', color = 'primary', onClick }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      class={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full transition-all duration-200 active:scale-[0.98] ${COLOR_CLASS[color]}`}
+      onclick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
