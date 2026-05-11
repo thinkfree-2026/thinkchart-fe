e999469d42555de71c7d2a146ddababe347c0524
10hajin15 <10hajin15@gmail.com>
2026-05-11 16:45:37 +0900

    feat: 웹 소켓 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Canvas } from './canvas/index.ts';
+import { Sidebar } from './components/sidebar/index.ts';
+import { connectWebSocket } from './sockets/index.ts';
 
 export const App = () => {
   return (
     <>
+      <Sidebar />
       <Canvas />
     </>
   );
 };
+
+connectWebSocket();
