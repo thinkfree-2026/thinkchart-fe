aadeed87f8c91abb2c65b1c49bc5d828ad9014fa
10hajin15 <10hajin15@gmail.com>
2026-05-11 16:02:39 +0900

    feat: web socket 세팅

---
+++ src/sockets/chartSocket.ts
@@ -0,0 +1,20 @@
+import { websocketClient } from './stompClient.ts';
+
+import type { StompSubscription } from '@stomp/stompjs';
+
+let currentChartSession: StompSubscription | null = null;
+
+export const chartSocket = {
+  enterChartSession: (chartId: string) => {
+    chartSocket.leaveChartSession();
+
+    currentChartSession = websocketClient.subscribe(`/topic/canvas/${chartId}`, () => {});
+  },
+
+  leaveChartSession: () => {
+    if (!currentChartSession) return;
+
+    currentChartSession.unsubscribe();
+    currentChartSession = null;
+  },
+};
