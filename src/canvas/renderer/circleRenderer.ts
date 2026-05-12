be5d9d80345e86592267c2300239ae96675c3127
Donghyun Lee <61397627+shubug1015@users.noreply.github.com>
2026-05-12 10:23:43 +0900

    feat: 원 최대 크기 테두리 렌더링 로직 작성

--- src/canvas/renderer/circleRenderer.ts
+++ src/canvas/renderer/circleRenderer.ts
@@ -1,4 +1,4 @@
-import { CIRCLE_BORDER_COLOR } from '../constants/index.ts';
+import { CIRCLE_BORDER_COLOR, MAX_VALUE } from '../constants/index.ts';
 import type { Circle } from '../types/index.ts';
 
 // 개별 원과 기본 테두리를 캔버스에 그리는 전담 함수
@@ -10,16 +10,37 @@ export const drawCircle = (
   isSelected: boolean,
   isGuide: boolean = false
 ) => {
+  // 원 생성
   ctx.beginPath();
   ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-
   ctx.fillStyle = circle.color;
   ctx.fill();
 
-  // 마우스 상호작용이 없고 가이드 원이 아닌 기본 상태의 원에만 테두리 렌더링
+  // 원 내부 텍스트
+  const fontSize = Math.max(14, circle.value / 2);
+  ctx.font = `bold ${fontSize}px "Noto Sans", sans-serif`;
+  ctx.fillStyle = '#000000';
+  ctx.textAlign = 'center'; // Horizontal center
+  ctx.textBaseline = 'middle';
+  ctx.fillText(String(circle.value), circle.x, circle.y);
+
+  // 원 실선 테두리
   if (!isHovered && !isSelected && !isGuide) {
-    ctx.lineWidth = 0.75;
+    ctx.lineWidth = 1;
     ctx.strokeStyle = CIRCLE_BORDER_COLOR;
     ctx.stroke();
+  }
+
+  if (circle.value >= MAX_VALUE) {
+    // 원 점선 테두리
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius + 10, 0, Math.PI * 2);
+    ctx.strokeStyle = 'rgba(129, 140, 248, 0.7)';
+    ctx.lineWidth = 5;
+    ctx.setLineDash([10, 5]);
+    ctx.lineCap = 'butt';
+    ctx.stroke();
+
+    ctx.setLineDash([]);
   }
 };
