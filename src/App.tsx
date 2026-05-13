6406d675b56c68166a003ff182acae21d24222df
10hajin15 <10hajin15@gmail.com>
2026-05-13 15:49:42 +0900

    feat: 차트 리스트 api 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { api, type ApiResponse } from './api/http.ts';
 import { Canvas } from './canvas/index.ts';
 import { Sidebar } from './components/sidebar/index.ts';
 import { connectWebSocket } from './sockets/index.ts';
+import { chartListState } from './store/index.ts';
+import type { ChartList } from './types/api/chartAPI.ts';
+
+const getChartList = async () => {
+  const res: ApiResponse<ChartList> = await api.get('/canvas/charts');
+  chartListState.charts = res.data;
+};
 
 export const App = () => {
+  getChartList();
+
   return (
     <>
       <Sidebar />
